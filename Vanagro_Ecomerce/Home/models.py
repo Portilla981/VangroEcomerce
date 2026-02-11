45e10928ecda6f4a3cb99b07c47d610478c1fc81
Portilla81 <jdportilla81@gmail.com>
2026-02-11 00:52:52 -0500

    Se realizo mensajeria de errores

--- Vanagro_Ecomerce/Home/models.py
+++ Vanagro_Ecomerce/Home/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Mensaje(models.Model):
-	fecha_envio= models.DateTimeField('Fecha de envio', auto_now_add=True)
+	fecha_envio= models.DateTimeField('Fecha de envió', auto_now_add=True)
 	nombres = models.CharField('Nombres', max_length=50)
 	apellido = models.CharField('Apellido', max_length=70)
-	telefono = models.PositiveBigIntegerField('Telefono', max_length=20)
-	email = models.EmailField('Correo Electronico')
+	telefono = models.PositiveBigIntegerField('Teléfono')
+	email = models.EmailField('Correo Electrónico')
 	mensaje = models.TextField('Su mensaje')
+	estado = models.BooleanField('Estado del mensaje', default=False)
  	
 	def __str__(self):
-		return f'{self.nombres} - {self.apellido}'
+		return f'{self.fecha_envio} -{self.nombres} - {self.apellido} - {self.estado}'
